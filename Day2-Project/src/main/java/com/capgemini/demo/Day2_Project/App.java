c56d0e12b4f1f11b870ba90222e8eb7303fd6635
syenakat <syenakat@DIN80011815.corp.capgemini.com>
2018-03-30 10:31:13 +0530

    git commit demo

---
+++ Day2-Project/src/main/java/com/capgemini/demo/Day2_Project/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.demo.Day2_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Day2-Project/src/main/java/com/capgemini/demo/Day2_Project/SalesClass.java
@@ -0,0 +1,12 @@
+package com.capgemini.demo.Day2_Project;
+
+	public class SalesClass {
+		
+		public void show(){
+			int[] arg={12,45,67,90};
+			
+			System.out.println("Show Method");
+		}
+
+	
+	}
